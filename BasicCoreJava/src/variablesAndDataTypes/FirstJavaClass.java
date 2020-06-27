f7dd149268dc9a974dc8e96d54b89a69ac590d94
prachee1010 <pracheeghumatkar@gmail.com>
2020-06-27 13:41:08 +0530

    first java project

---
+++ BasicCoreJava/src/variablesAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package variablesAndDataTypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Automation!");
+	}
+
+}
