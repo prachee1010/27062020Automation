22a475cf2f45305604ee5c06ceecf3598de6a673
prachee1010 <pracheeghumatkar@gmail.com>
2020-07-27 13:15:02 +0530

    26072020 files

--- SeleniumProject/src/BasicSeleniumFunctions/LaunchBrowser.java
+++ SeleniumProject/src/BasicSeleniumFunctions/LaunchBrowser.java
@@ -1,5 +1,8 @@
 package BasicSeleniumFunctions;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchBrowser {
@@ -10,7 +13,24 @@ public class LaunchBrowser {
 		System.setProperty("webdriver.chrome.driver", "D:\\Tech Bodhi\\Automation Notes\\Drivers\\chromedriver.exe");
 		
 		ChromeDriver dr = new ChromeDriver();
-		dr.get("https://www.google.com/");
+//		dr. manage().window().maximize();
+		dr.get("http://sampleapp.tricentis.com/101/");
+		
+/*		Click
+		dr.findElement(By.id("nav_automobile")).click();
+*/		
+		//Get Text
+		WebElement ele = dr.findElement(By.id("nav_automobile"));
+		String text = ele.getText();
+		System.out.println(text);
+		
+		//Enter Text
+		dr.findElement(By.id("search_form")).sendKeys("VW");
+		dr.findElement(By.id("search_form")).sendKeys(Keys.ENTER);
+		
+		
+//		dr.close();		//closes current window launched by driver
+//		dr.quit();		//quits all current sessions launched by driver
 	}
 
 }
